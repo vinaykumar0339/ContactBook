114280fa78e0da17907dd15d69db834898ae015a
vinaykumar0339 <66993050+vinaykumar0339@users.noreply.github.com>
2024-10-21 18:56:50 +0530

    contact book console csharp application

---
+++ Santhosh_Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Santhosh_Assignment.ContactsManager;
+
+namespace Santhosh_Assignment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ContactsManager.ContactsManager contactsManager = new ContactsManager.ContactsManager();
+            contactsManager.run();
+        }
+    }
+}
